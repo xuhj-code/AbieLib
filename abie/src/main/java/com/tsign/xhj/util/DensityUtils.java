69a7c19fac11cfd1f893d39224c5eb246321202f
haru <1195472325@qq.com>
2017-03-15 18:14:24 +0800

    去除轮播的adapter，添加轮播的布局（去除的adapter放在以后项目中用），添加DensityUtils

---
+++ abie/src/main/java/com/tsign/xhj/util/DensityUtils.java
@@ -0,0 +1,33 @@
+package com.tsign.xhj.util;
+
+import android.content.Context;
+
+/**
+ * 2016年11月4日17:02:42
+ *
+ * @author haru
+ */
+public class DensityUtils {
+
+    // 转换dip为px
+    public static int dip2px(Context context, int dip) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dip * scale + 0.5f * (dip >= 0 ? 1 : -1));
+    }
+
+    // 转换px为dip
+    public static int px2dip(Context context, int px) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (px / scale + 0.5f * (px >= 0 ? 1 : -1));
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int px2sp(Context context, float pxValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+}
