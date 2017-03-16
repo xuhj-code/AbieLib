4c61be2419829627bc86f61de384e0c956c27087
haru <1195472325@qq.com>
2017-03-16 13:45:44 +0800

    添加camera，人脸检测

--- abie/src/main/java/com/tsign/xhj/util/DensityUtils.java
+++ abie/src/main/java/com/tsign/xhj/util/DensityUtils.java
@@ -1,6 +1,10 @@
 package com.tsign.xhj.util;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
 
 /**
  * 2016年11月4日17:02:42
@@ -8,6 +12,7 @@ import android.content.Context;
  * @author haru
  */
 public class DensityUtils {
+    private static final String TAG = "DisplayUtil";
 
     // 转换dip为px
     public static int dip2px(Context context, int dip) {
@@ -29,5 +34,33 @@ public class DensityUtils {
     public static int px2sp(Context context, float pxValue) {
         float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
         return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 获取屏幕宽度和高度，单位为px
+     *
+     * @param context
+     * @return
+     */
+    public static Point getScreenMetrics(Context context) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        int w_screen = dm.widthPixels;
+        int h_screen = dm.heightPixels;
+        Log.i(TAG, "Screen---Width = " + w_screen + " Height = " + h_screen + " densityDpi = " + dm.densityDpi);
+        return new Point(w_screen, h_screen);
+
+    }
+
+    /**
+     * 获取屏幕长宽比
+     *
+     * @param context
+     * @return
+     */
+    public static float getScreenRate(Context context) {
+        Point P = getScreenMetrics(context);
+        float H = P.y;
+        float W = P.x;
+        return (H / W);
     }
 }
