6571c2f1ce6870d6bc622fdd5eae1bc9fcc59f7b
haru <1195472325@qq.com>
2017-03-20 18:13:11 +0800

    工具类增加新的方法

--- abie/src/main/java/com/tsign/xhj/util/HaruUtil.java
+++ abie/src/main/java/com/tsign/xhj/util/HaruUtil.java
@@ -1,15 +1,136 @@
 package com.tsign.xhj.util;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
+
+import java.io.File;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.HashMap;
 
 /**
  * Created by haru on 2017/3/18.
  */
 
 public class HaruUtil {
+
+    private static String TAG = "HaruUtil";
+
+    /**
+     * 2016年12月3日15:36:10
+     * 轮询实现心跳包
+     *
+     * @return
+     */
+
+    public static void startPollingService(Context context, HashMap<String, String> map, int seconds, Class<?> cls, String action) {
+        AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, cls);
+        if (null != map) {
+            intent.putExtra("map", (Serializable) map);
+        } else {
+            map = new HashMap<>();
+            map.put("error", "error");
+            intent.putExtra("map", (Serializable) map);
+        }
+        intent.setAction(action);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        long triggerAtTime = SystemClock.elapsedRealtime();
+        manager.setRepeating(AlarmManager.ELAPSED_REALTIME, triggerAtTime, seconds * 1000, pendingIntent);
+    }
+
+    public static void stopPollingService(Context context, Class<?> cls, String action) {
+        AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, cls);
+        intent.setAction(action);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        manager.cancel(pendingIntent);
+    }
+
+    /**
+     * 2016-12-13 13:17:34
+     *
+     * @param file
+     */
+    public static void deleteFile(File file) {
+        if (file.exists()) { // 判断文件是否存在
+            if (file.isFile()) { // 判断是否是文件
+                file.delete(); // delete()方法 你应该知道 是删除的意思;
+            }
+            file.delete();
+            Log.e(TAG, file + "已删除");
+        } else {
+            Log.e(TAG, "没有此文件");
+        }
+    }
+
+    /**
+     * 提供精确加法计算的add方法
+     *
+     * @param value1 被加数
+     * @param value2 加数
+     * @return 两个参数的和
+     */
+    public static double add(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
+        BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+        return b1.add(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确减法运算的sub方法
+     *
+     * @param value1 被减数
+     * @param value2 减数
+     * @return 两个参数的差
+     */
+    public static double sub(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
+        BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+        return b1.subtract(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确乘法运算的mul方法
+     *
+     * @param value1 被乘数
+     * @param value2 乘数
+     * @return 两个参数的积
+     */
+    public static double mul(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
+        BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+        return b1.multiply(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的除法运算方法div
+     *
+     * @param value1 被除数
+     * @param value2 除数
+     * @param scale  精确范围
+     * @return 两个参数的商
+     * @throws IllegalAccessException
+     */
+    public static double div(double value1, double value2, int scale) throws IllegalAccessException {
+        //如果精确范围小于0，抛出异常信息
+        if (scale < 0) {
+            throw new IllegalAccessException("精确度不能小于0");
+        }
+        BigDecimal b1 = new BigDecimal(Double.valueOf(value1));
+        BigDecimal b2 = new BigDecimal(Double.valueOf(value2));
+        return b1.divide(b2, scale).doubleValue();
+    }
+
     /**
      * 开启View闪烁效果
      */
--- abie/src/main/java/com/tsign/xhj/util/NetworkState.java
+++ abie/src/main/java/com/tsign/xhj/util/NetworkState.java
@@ -34,14 +34,12 @@ public class NetworkState {
     // 检查是否连接到网络
     // whether connect to internet
     public static boolean networkConnected(Context context) {
-
         if (context != null) {
             ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo info = manager.getActiveNetworkInfo();
             if (info != null)
                 return info.isAvailable();
         }
-
         return false;
     }
 
